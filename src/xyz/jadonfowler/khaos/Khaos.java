188fd5a95ed5e74d2916acdd861222dcb213d394
Jadon Fowler <jadonflower@gmail.com>
2016-06-01 15:37:22 -0700

    Initial Commit
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

---
+++ src/xyz/jadonfowler/khaos/Khaos.java
@@ -0,0 +1,17 @@
+package xyz.jadonfowler.khaos;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Khaos extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+    
+}
